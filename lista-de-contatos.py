9bd4b026ffa2e1ea405a333c99a184b931c37df3
Alphynha <monteiroalfa3@gmail.com>
2024-04-29 10:28:59 -0300

    Adicionando função de Busca de Contatos

--- lista-de-contatos.py
+++ lista-de-contatos.py
@@ -1,9 +1,7 @@
-#Função Menu Principal
-
 #Função Adicionar contato
 def adicionarContato(lista):
     try:
-        nome = input("Digite o nome do contato: ").lower()
+        nome = input("Digite o nome do contato: ").title()
         numero = int(input("Digite o numero do contato: "))
         lista[nome] = numero
         print("Contato adicionado com sucesso!\n")
@@ -29,16 +27,16 @@ def atualizarContatos(lista):
         escolha = input("Digite sua escolha ")
 
         if(escolha == '1'):
-            nomeAntigo = input('Digite o nome do contato que deseja atualizar: ').lower()
+            nomeAntigo = input('Digite o nome do contato que deseja atualizar: ').title()
             if nomeAntigo in lista:
-                novoNome = input("Digite o novo nome para atualizar: ").lower()    
+                novoNome = input("Digite o novo nome para atualizar: ").title()    
                 lista[novoNome] = lista.pop(nomeAntigo)
                 print("Seu contato foi atualizado!\n")
             else:
                 print("Contato não encontrado\n")
 
         elif(escolha == '2'):
-            nome = input("Digite o nome de do contato que deseja atualizar o número: ").lower()
+            nome = input("Digite o nome de do contato que deseja atualizar o número: ").title()
             if nome in lista:
                 novoNumero = int(input("Digite o novo número: "))
                 lista[nome] = novoNumero
@@ -57,7 +55,7 @@ def atualizarContatos(lista):
 #Função excluir contato:
 def excluirContato(lista):
     try:
-        nome = input('Digite o nome do contato que deseja excluir: ').lower()
+        nome = input('Digite o nome do contato que deseja excluir: ').title()
         if nome in lista:
             lista.pop(nome)
             print("O contato foi excluido com sucesso!\n")
@@ -67,7 +65,24 @@ def excluirContato(lista):
     except Exception as e:
         print(f"Erro: {e}\n")
 
-#Função principal:
+#Função Buscar Contatos:
+def buscarContatos(lista):
+    escolha = input("Digite o nome: ")
+    resultado = []
+
+    for nome, numero in lista.items():
+        if escolha.title() in nome.title():
+            resultado.append((nome, numero))
+
+    if resultado:
+        print("Aqui está o resultado da busca:")
+        for nome, numero in resultado:
+            print(f"Nome: {nome}, Número: {numero}")
+    
+    else:
+        print("Nenhum contato foi encontrado!")
+
+#Função menu principal:
 def menuPrincipal():
     try:
         lista = {}
@@ -79,7 +94,8 @@ def menuPrincipal():
             print("2. Excluir um contato")
             print("3. Atualizar um contato")
             print("4. Visualizar lista de contatos")
-            print("5. Finalizar o programa")
+            print("5. Buscar contatos")
+            print("6. Finalizar o programa")
             
             escolha = input("Digite o número da sua escolha: ")
 
@@ -91,9 +107,12 @@ def menuPrincipal():
                 atualizarContatos(lista)
             elif(escolha == '4'):
                 visualizarContatos(lista)
-            elif (escolha == '5'):
+            elif(escolha == '5'):
+                buscarContatos(lista)
+            elif (escolha == '6'):
                 print("Finalizando programa, aqui está sua lista!\n")
-                print(lista)
+                for nome, numero in lista.items():
+                    print(f"Nome: {nome}, Número: {numero}")
                 break
             else:
                 print("Escola inválida. Por favor, tente novamente!\n")
